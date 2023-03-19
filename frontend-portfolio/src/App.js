2a5dea2ca7662f5c9826e066fd2b99cd981ed1de
mishdgr8 <Mishaelmordi@gmail.com>
2023-03-19 13:53:17 +0100

    Navigation bar completed with fully responsiveness

--- frontend-portfolio/src/App.js
+++ frontend-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./containers";
 import { Navbar } from "./components";
+import "./app.scss";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      hhsvh
       <Header />
+      gvcadjha
       <About />
       <Work />
       <Skills />
